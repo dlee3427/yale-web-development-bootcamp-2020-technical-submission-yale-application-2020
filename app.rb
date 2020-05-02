1a6b0cbc1e6cd7f5bc0de83f8458ec4ae719239d
David Lee <david.lee.sl2634@yale.edu>
2020-05-02 20:57:47 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -7,6 +7,7 @@ favorite_food = "Steak"
 height_in_inches = 69
 height_message = (height_in_inches > AVERAGE_HEIGHT_IN_INCHES) ? ("You are taller than average.") : ("You are not taller than average.")
 
+
 ## Do not modify below this line!
 
 output(name, age, favorite_food, height_message)
